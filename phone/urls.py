1a45ca3749738a5a2691ee527022a48d669c7934
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-11-20 09:39:30 +0300

    Updated project urls for views of the iLanding tempalete

--- phone/urls.py
+++ phone/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Call import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('starter/', views.starter, name="Starter Page"),
+    path('service/', views.service, name="Service Details"),
 ]
