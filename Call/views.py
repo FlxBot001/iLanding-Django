898ac94dc5b52f40c41a3b9bfb1518e80b9b0400
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-11-20 09:34:25 +0300

    Installed app on settings and created view functions to render html files

--- Call/views.py
+++ Call/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+   return render(request, 'index.html')
+
+def starter(request):
+   return render(request, 'starter-page.html')
+
+def service(request):
+   return render(request, 'service-detail.html')
